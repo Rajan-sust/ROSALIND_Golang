559bae528ec302a099b87d0c36736f99bb5fa602
Rajan-sust <imrajon0@gmail.com>
2021-02-26 09:13:38 +0600

    ba1k time complexity  updated

--- ba1k.go
+++ ba1k.go
@@ -1,25 +1,39 @@
 package main
 
-import (
-  "fmt"
-  "RosalindGolang/utility"
-)
+import "fmt"
+
+func Pow(a, x int) int {
+  ans := 1
+  for i := 1; i <= x; i++ {
+    ans *= a
+  }
+  return ans
+}
+
+func SeqToNum(kmer string) int {
+  mp := map[byte]int{
+    'A' : 0,
+    'C' : 1,
+    'G' : 2,
+    'T' : 3,
+  }
+  num := 0
+  for _, bp := range kmer {
+    num = num * 4 + mp[byte(bp)]
+  }
+  return num
+}
 
 func FrequencyFind(text string, k int) []int {
   mp := map[string]int{}
   for i := 0; i < len(text) - k + 1; i++ {
     mp[text[i:i+k]]++
   }
-  arr := utility.GenerateKmer(k)
-  fre := []int{}
-  for _, kmer := range(arr) {
-    if val, ok := mp[kmer]; ok {
-      fre = append(fre, val)
-    } else {
-      fre = append(fre, val)
-    }
+  frequency := make([]int, Pow(4, k))
+  for k, v := range mp {
+    frequency[SeqToNum(k)] = v
   }
-  return fre
+  return frequency
 }
 
 func FormattedPrint(frequency []int) {
@@ -38,6 +52,7 @@ func FormattedPrint(frequency []int) {
 func main() {
   var text string
   var k int
+  // fmt.Print(SeqToNum("AAAA"))
   fmt.Scan(&text, &k)
   FormattedPrint(FrequencyFind(text, k))
 }
